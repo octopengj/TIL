262e9c745ffe8a5c7d8dea461fd0244f7dad3d18
octopengj <octopengj@google.com>
2021-03-30 16:16:44 +0900

    animation

---
+++ JAVASCRIPT/animation/animation.js
@@ -0,0 +1,9 @@
+const ballElem = document.querySelector(".ball");
+
+ballElem.addEventListener("click", function () {
+  ballElem.style.animation = "ani 1s 3 alternate forwards";
+});
+
+ballElem.addEventListener("animationend", function () {
+  ballElem.classList.add("move");
+});
